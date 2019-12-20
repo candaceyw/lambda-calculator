b97bb1583f54e99fa14e1a45530d9c5376d897c1
Candace Wilson <candacewilson@Candaces-MacBook-Air.local>
2019-12-19 23:39:16 -0500

    buttons now showing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
+
 // STEP 4 - import the button and display components
+import Numbers from "./components/ButtonComponents/NumberButtons/Numbers.js";
+import Operators from "./components/ButtonComponents/OperatorButtons/Operators.js";
+import Specials from "./components/ButtonComponents/SpecialButtons/Specials.js";
+import Display from "./components/DisplayComponents/Display.js";
+
 // Don't forget to import any extra css/scss files you build into the correct component
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
+import { specials } from "./data";
 
-function App() {
+function App(props) {
+  const [] = useState()
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
   // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
@@ -17,7 +25,13 @@ function App() {
     <div className="container">
       <Logo />
       <div className="App">
-        {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+      <div>
+          <Specials />
+          <Numbers />
+          {/* <Numbers onButtonPress={numberHandler} /> */}
+        </div>
+        {/* <Operators onButtonPress={operatorHandler} /> */}
+        <Operators />
       </div>
     </div>
   );
--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,14 @@
 import React from "react";
+// import {operators} from '../../../data';
 
-const NumberButton = () => {
+const NumberButton = (props) => {
+ 
   return (
     <>
+      <button>{props.number}</button>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
     </>
   );
 };
+
+export default NumberButton;
