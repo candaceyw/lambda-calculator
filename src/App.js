67122aaea884a3859829520788a40d7ac94ae8ab
Candace Wilson <candacewilson@Candaces-MacBook-Air.local>
2020-01-07 08:16:27 -0500

    buttons functioning

--- src/App.js
+++ src/App.js
@@ -1,51 +1,33 @@
-import React, {useState} from "react";
-import "./App.scss";
+import React, { useState } from 'react';
+import './App.scss';
+import Header from './components/Header';
+import Calculator from './components/Calculator/Calculator';
 
-// STEP 4 - import the button and display components
-import Numbers from "./components/ButtonComponents/NumberButtons/Numbers.js";
-import Operators from "./components/ButtonComponents/OperatorButtons/Operators.js";
-import Specials from "./components/ButtonComponents/SpecialButtons/Specials.js";
-import Display from "./components/DisplayComponents/Display.js";
+function App() {
+	const [ history, setHistory ] = useState([]);
+	return (
+		<div className="wrapper">
+			<div className="history">
+				<h1>History</h1>
 
-// Don't forget to import any extra css/scss files you build into the correct component
-
-// Logo has already been provided for you. Do the same for the remaining components
-import Logo from "./components/DisplayComponents/Logo";
-// import { specials } from "./data";
-
-function App(props) {
-  // const [] = useState()
-  // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
-  // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
-  // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should receive 5 if the user clicks on
-  // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
-  // Don't forget to pass the functions (and any additional data needed) to the components as props
-
-  return (
-    <div className="container">
-      <Logo />
-      <Display />
-    
-    <div className="button-wrapper">
-      <div className="App">
-
-        <div className='btnWrapper'>
-          <div className="specButtons">
-          <Specials />
-          </div>
-          <div className="numsButtons">
-          <Numbers />
-          </div>
-        </div>
-        
-      </div>
-
-      <div className="opButtons">
-        <Operators />
-        </div>
-        </div>
-      </div>
-  );
+				{history.map((item, idx) => {
+					return (
+						<p key={idx}>
+							{item.problem}={item.solution}
+						</p>
+					);
+				})}
+				<button id="historyButton" onClick={() => setHistory([])}>
+					Clear History
+				</button>
+			</div>
+			<div className="container">
+				<div className="App" />
+				<Header />
+				<Calculator setHistory={setHistory} />
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/DisplayComponents/Display.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Display = (props) => {
-  return <div className="display">{props.value}</div>;
-};
-
-export default Display;
